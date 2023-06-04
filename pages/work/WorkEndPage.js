a2c183c7a23f1656c9f38a25fb7b8385ec883ba8
BlueApple856729 <126082098+BlueApple865729@users.noreply.github.com>
2023-06-03 21:58:19 -0800

    work end page complete
    Co-authored-by: YoshinoHaruki btsm0714@gmail.com

--- pages/work/WorkEndPage.js
+++ pages/work/WorkEndPage.js
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 import { css } from '../../style';
+import Footer from '../../components/Footer';
 
 import img_logo from '../../assets/logo2.png';
 import img_mid from '../../assets/work_end.png';
 
 const styles = StyleSheet.create({
+    cont: {
+        backgroundColor: 'white',
+    },
     img_logo: {
         width: 355,
         height: 141,
@@ -13,49 +17,65 @@ const styles = StyleSheet.create({
         marginRight: 'auto',
         marginTop: 47,
     },
+    txt1: {
+        color: '#303030',
+        fontSize: 22,
+        marginTop: 26,
+        textAlign: 'center',
+    },
+    txt2: {
+        color: '#303030',
+        fontSize: 37,
+        marginTop: 9,
+        textAlign: 'center',
+    },
+    txt3: {
+        color: '#303030',
+        fontSize: 22,
+        marginTop: 3,
+        textAlign: 'center',
+    },
     img_mid: {
-        width: 336,
-        height: 323,
+        width: 287,
+        height: 215,
         marginLeft: 'auto',
         marginRight: 'auto',
-        marginTop: 34,
-    },
-    ttl: {
-        marginTop: 40,
-    },
-    dsc: {
-        marginTop: 13
     },
     btn_yellow: {
-        marginTop: 43,
-    },
-    btn_login: {
-        width: 300,
-        height: 52,
-        color: 'white',
-        fontSize: 16,
-        backgroundColor: '#2C92D2',
-        border: '2px solid white',
+        backgroundColor: '#FFC602',
         borderRadius: 5,
-        fontWeight: 'bold',
+        width: 300,
+        height: 72,
+        marginTop: 26,
         display: 'flex',
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: 'center',
         marginLeft: 'auto',
         marginRight: 'auto',
-        marginTop: 26,
     },
+    txt_btn_1: {
+        fontSize: 16,
+        color: 'black',
+    },
+    txt_btn_2: {
+        color: '#676767',
+        fontSize: 12,
+    }
 });
 
 export default function WorkEndPage(props) {
     return (
-        <View style={css.cont_blue}>
+        <View style={[css.cont_white, styles.cont]}>
             <Image source={img_logo} style={styles.img_logo} />
+            <Text style={styles.txt1}>報酬が確定しました！</Text>
             <Image source={img_mid} style={styles.img_mid} />
-            <Text style={[css.ttl, styles.ttl]}>JOBiへようこそ！</Text>
-            <Text style={[css.dsc, styles.dsc]}>登録してJOBiをはじめよう！</Text>
-            <View style={[css.btn_yellow, styles.btn_yellow]}>新規登録</View>
-            <View style={styles.btn_login}>ログイン</View>
+            <Text style={styles.txt2}>8,000円</Text>
+            <Text style={styles.txt3}>お疲れ様でした。</Text>
+            <View style={styles.btn_yellow}>
+                <Text style={styles.txt_btn_1}>振り込み予定を見る</Text>
+                <Text style={styles.txt_btn_2}>マイページへ</Text>
+            </View>
+            <Footer num={3} />
         </View >
     );
 }
